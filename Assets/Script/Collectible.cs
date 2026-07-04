06e59b6df6a9434a304a503b7993f00df768a00c
Ke-no <S10275096@connect.np.edu.sg>
2026-07-04 22:33:59 +0800

    Add tunnels and domain with collectibles

--- Assets/Script/Collectible.cs
+++ Assets/Script/Collectible.cs
@@ -1,16 +1,26 @@
 using UnityEngine;
 
-public class Collectible : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+/*
+*Author: Keno
+*Date: 14/6/2026
+*Description: Player able to interact and collect items
+*/
 
-    // Update is called once per frame
-    void Update()
+/*increment item count*/
+public class Collectibles : Interactable
+{
+    [SerializeField]
+    private bool isEgg = false;
+    protected override void Interact()
     {
-        
+/*player clicks E, collect item*/
+        GameManager.Instance.CollectItem();
+/*if its egg, count separately*/
+        if (isEgg)
+        {
+            GameManager.Instance.CollectEgg();
+        }
+/*delete item from game*/     
+        Destroy(gameObject);
     }
 }
